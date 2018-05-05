b93162ad13d1c7442262c33570f0c7638b97ae48
Maayan Elgamil <maayan.elgamil@gmail.com>
2018-05-05 17:35:47 +0300

    iterate files

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import numpy
+from pylab import *
+import urllib2
+from bs4 import BeautifulSoup
+import os
 
+sourcePath = "E:\sentiment analisis\\"
+data = []
 
-print("hello world")
+def read_files():
+    for filename in os.listdir(sourcePath):
+       print(filename)
+       data = pd.read_excel(sourcePath+filename)
+
+read_files()
