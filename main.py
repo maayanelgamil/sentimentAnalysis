49c877bf0448304a718896818a8ef0bf0fab01c5
Maayan Elgamil <maayan.elgamil@gmail.com>
2018-05-06 22:39:42 +0300

    add finder for all the 'p' tags and aggregate them

--- main.py
+++ main.py
@@ -5,22 +5,40 @@ import urllib2
 from bs4 import BeautifulSoup
 import os
 
+# Header to simulate that we request as a user
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'}
+
 sourcePath = "E:\sentiment analisis\\"
-data = []
 
 # need to think about how to find the actual artical div
 def read_files():
+    allData = pd.DataFrame()
     for filename in os.listdir(sourcePath):
        data = pd.read_excel(sourcePath+filename)
-       #links = data.loc['URLID','URLString','Title']
-       for index, link in data.iterrows():
-           if(index > 0):
-                print(link['URLString'])
-                response = urllib2.urlopen(link['URLString'])
-                soup = BeautifulSoup(response, 'html.parser')
-                title = soup.body.findAll(text=link['Title']).pop()
-                print(title)
-                article = title.find_next_sibling("div")
-                print(article)
+       allData = allData.append(data)
+    return allData
 
-read_files()
+
+def get_html_article_from_url(allData):
+    # add the new file to the existing data we have
+    for index, link in allData.iterrows():
+            try:
+                print(link['URLString'])
+                req = urllib2.Request(link['URLString'], None, headers)
+                response = urllib2.urlopen(req, timeout=5).read()
+                soup = BeautifulSoup(response, 'html.parser')
+                #title = soup.body.find_all(text=tmpTitle, recursive=True).pop().parent
+                #print(title)
+                #articleDivs = title.findNext("div")
+                links = [e.get_text() for e in soup.body.find_all('p', recursive=True)]
+                article = '\n'.join(links)
+                print(article)
+                allData['articl_dirty_text'] = article
+            except Exception, e:
+                print(e)
+    writer = pd.ExcelWriter('allData.xlsx')
+    allData.to_excel(writer, 'Sheet1')
+    writer.save()
+
+allData = read_files()
+get_html_article_from_url(allData)
